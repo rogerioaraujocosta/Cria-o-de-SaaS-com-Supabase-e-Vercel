409568d0f8b5923e015aeccbf56c534bbc17c0b9
rogerioaraujocosta <araujocosta.rogerio@gmail.com>
2025-04-19 18:17:15 -0300

    feat: add FormDescription stub

--- components/ui/form.tsx
+++ components/ui/form.tsx
@@ -1,25 +1,26 @@
+// components/ui/form.tsx
 import React from 'react';
 
 export function Form(props: React.FormHTMLAttributes<HTMLFormElement>) {
   return <form {...props}>{props.children}</form>;
 }
-
 export function FormField(props: React.HTMLAttributes<HTMLDivElement>) {
   return <div {...props}>{props.children}</div>;
 }
-
 export function FormItem(props: React.HTMLAttributes<HTMLDivElement>) {
   return <div {...props}>{props.children}</div>;
 }
-
 export function FormLabel(props: React.LabelHTMLAttributes<HTMLLabelElement>) {
   return <label {...props}>{props.children}</label>;
 }
-
 export function FormControl(props: React.InputHTMLAttributes<HTMLInputElement>) {
   return <input {...props} />;
 }
-
 export function FormMessage(props: React.HTMLAttributes<HTMLDivElement>) {
+  return <div {...props}>{props.children}</div>;
+}
+
+// Adicione isto:
+export function FormDescription(props: React.HTMLAttributes<HTMLDivElement>) {
   return <div {...props}>{props.children}</div>;
 }
